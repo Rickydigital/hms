7886f71af10c355076d6fadbe5b38e54256f53a2
ricky <erikikinyamagoha@gmail.com>
2025-11-30 01:24:13 +0300

    script

--- resources/views/pharmacy/sales/create.blade.php
+++ resources/views/pharmacy/sales/create.blade.php
@@ -190,9 +190,7 @@
         </div>
     </form>
 </div>
-@endsection
 
-@push('scripts')
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script>
 let itemIndex = 1;
@@ -282,4 +280,5 @@ function updateTotals() {
     $('#changeDue').text('Tsh 0');
 }
 </script>
-@endpush
+@endsection
+
